2003114f0350a037926cfdaa811eac59050b1f7b
Themistocles Papavramidis <110047465+themispap@users.noreply.github.com>
2024-11-12 23:19:03 +0200

    Update lab04.s

--- lab04.s
+++ lab04.s
@@ -26,10 +26,19 @@ arrayNotSorted: .word marianna, markos, maria
             ecall
 
 str_ge:
-#---------
-# Write the subroutine code here
-#  You may move jr ra   if you wish.
-#---------
+            lbu  t0, 0(a0)
+            lbu  t1, 0(a1)
+            sub  t2, t0,   t1  # result: 0 if equal,
+                               # >0 if char @a0 "higher", <0 if char @a1 "higher"
+            addi a0, a0,   1
+            addi a1, a1,   1
+            add  t3, t1,   t0  # if the sum is equal to either one, one must be 0
+            beq  t3, t0,   ret_strcmp  # any string finished, leave
+            # I could have compared each of t0, t1 to zero. Still 2 instructions (beq's)
+            beq  t2, zero, str_ge  #still equal, loop
+ret_strcmp:
+            srli a0, t2, 31  # get the sign bit. If 1, negative, so strictly less
+            xori a0, a0, 1 # Invert it. So a0 is 1 if greater or equal.
             jr   ra
  
 # ----------------------------------------------------------------------------
@@ -42,8 +51,26 @@ str_ge:
 #     return 0
 
 recCheck:
-#---------
-# Write the subroutine code here
-#  You may move jr ra   if you wish.
-#---------
+            slti t0, a1,   2
+            beq  t0, zero, checkFirstTwo
+            addi a0, zero, 1  # return 1
+            jr   ra
+checkFirstTwo:
+            addi sp, sp,   -12
+            sw   ra, 8(sp)
+            sw   a0, 4(sp)
+            sw   a1, 0(sp)
+            lw   a1, 0(a0)  # 1st
+            lw   a0, 4(a0)  # 2nd 
+            jal  str_ge
+            beq  a0, zero, return  # return 0, a0 is already 0
+            # do recursion
+            lw   a0, 4(sp)    # get original a0, a1 from stack
+            lw   a1, 0(sp)
+            addi a0, a0,   4   # check the rest of the array, after 1st element.
+            addi a1, a1,   -1  # size-1
+            jal  recCheck
+return:
+            lw   ra, 8(sp)
+            addi sp, sp,   12
             jr   ra
